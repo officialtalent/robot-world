5756fb487e79f684d2d11b9f75ea8a7c021c1bc1
officialtalent <talentsqwabe@gmail.com>
2022-05-23 21:21:22 +0200

    Position class added and unit tests for Position.

---
+++ src/test/java/com/devhouseagency/robotworld/PositionTest.java
@@ -0,0 +1,38 @@
+package com.devhouseagency.robotworld;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PositionTest {
+    @Test
+    void testPositionGetX() {
+        Position position = new Position(1, 5);
+        assertEquals(1, position.getX());
+    }
+
+    @Test
+    void testPositionGetY() {
+        Position position = new Position(1, 5);
+        assertEquals(5, position.getY());
+    }
+
+    @Test
+    void testEquals() {
+        Position pos1 = new Position(1, 5);
+        Position pos2 = new Position(1, 5);
+        assertTrue(pos1.equals(pos2));
+    }
+
+    @Test
+    void isInsideBounds() {
+        Position topLeft = new Position(-50, 50);
+        Position bottomRight = new Position(50, -50);
+
+        Position pos1 = new Position(35, -35);
+        assertTrue(pos1.isIn(topLeft, bottomRight));
+
+        Position pos2 = new Position(-100, 45);
+        assertFalse(pos2.isIn(topLeft, bottomRight));
+    }
+}
